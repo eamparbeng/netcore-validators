42e6de105915f0563e65434e4442f549a612f05a
Kofi Amparbeng <kofi@acsghana.com>
2020-10-12 11:09:45 +0000

    First Pass Implementation of Name Validator

--- NamesValidator/NamesValidatorImplementation.cs
+++ NamesValidator/NamesValidatorImplementation.cs
@@ -1,8 +1,65 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace NamesValidator
 {
     public class NamesValidatorImplementation
     {
+        private const int MIN_ACCEPTABLE_REPEATED_CONSONANTS_OR_VOWELS = 2;
+
+        private Func<char, bool> isVowel = (char ch) =>
+            ch == 'A' || ch == 'a' || ch == 'e' || ch == 'E'
+            || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O'
+            || ch == 'u' || ch == 'U';
+
+        public bool IsValidName(string nameCandidate)
+        {
+            if (HasRepeatedConsonants(nameCandidate))
+                return false;
+
+            if (HasRepeatedVowels(nameCandidate))
+                return false;
+
+            return true;
+        }
+
+        private bool HasRepeatedConsonants(string nameCandidate)
+        {
+            for (var i = 0; i < nameCandidate.Length; i++)
+            {
+                int consonantCount = 0;
+                for (var j = i; j < nameCandidate.Length; j++)
+                {
+                    if (!isVowel(nameCandidate[j]))
+                        consonantCount++;
+                    else
+                        break;
+                }
+                if (consonantCount > MIN_ACCEPTABLE_REPEATED_CONSONANTS_OR_VOWELS)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private bool HasRepeatedVowels(string nameCandidate)
+        {
+            for (var i = 0; i < nameCandidate.Length; i++)
+            {
+                int vowelCount = 0;
+                for (var j = i; j < nameCandidate.Length; j++)
+                {
+                    if (isVowel(nameCandidate[j]))
+                        vowelCount++;
+                    else
+                        break;
+                }
+                if (vowelCount > MIN_ACCEPTABLE_REPEATED_CONSONANTS_OR_VOWELS)
+                    return true;
+            }
+
+            return false;
+        }
     }
 }
